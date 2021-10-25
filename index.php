afd2a8f5404c26ef7ad7d70a96d4ff8cab0fd7f3
abduljeleelng <abdi=uljeleelng@gmail.com>
2021-10-25 15:02:16 +0100

    footer, header and index for entry services

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<?php get_header(); ?>
+<?php 
+if(have_posts()){
+    while(have_posts()){
+        the_post();
+        echo "<h2><a href='the_permalink()'> the_title() </a></h2>";
+    }
+}else{
+    echo "<p> No post to request for ";
+}
+
+?>
+<?php get_footer(); ?>
