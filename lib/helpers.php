274f90dee88a25009d884695edafddad3433ffef
abduljeleelng <abdi=uljeleelng@gmail.com>
2021-10-26 11:19:16 +0100

    hlpers function in lib folder

---
+++ lib/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+function firsttheme_post_meta(){
+    ?>
+      posted on <a href="<?php echo get_permalink() ?>">
+                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('l, F j, Y'); ?> </time>
+             </a>
+               By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
+             <?php echo get_the_author() ?> 
+           </a>
+    <?php
+}
+
+function firsttheme_readmore_link(){
+  ?>
+  <a href="<?php echo get_the_permalink(); ?>" title="<?php the_title_attribute(); ?>" > 
+   Read More  <span class="u-screen-reader-text">  About <?php the_title()  ?> </span>
+ </a>
+<?php
+}
+
+
