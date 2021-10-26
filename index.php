878f772d960d9f2c5c7434d36220646f2f5af7a0
abduljeleelng <abdi=uljeleelng@gmail.com>
2021-10-26 11:20:03 +0100

    modularize template tag

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 <?php 
+//  $custom = new WP_Query();
+//     echo "<pre>";
+//     var_dump($custom);
+//  echo "</pre>";
+
+?>
+<?php 
+   
     if(have_posts()){
         while(have_posts()){
             the_post();
@@ -8,20 +16,13 @@
                 <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"> <?php the_title(); ?> </a>
             </h2>
             <div>
-                posted on <a href="<?php echo get_permalink() ?>">
-                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('l, F j, Y'); ?> </time>
-             </a>
-             By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
-             <?php echo get_the_author() ?> 
-            </a>
+              <?php firsttheme_post_meta() ?>
             </div>
             <div>
                 <?php  the_excerpt(); ?>
             </div>
-            <a href="<?php echo get_the_permalink(); ?>" title="<?php the_title_attribute(); ?>" > 
-             Read More  <span class="u-screen-reader-text">  About <?php the_title()  ?> </span>
-            </a>
         <?php
+         firsttheme_readmore_link();
         }
         the_posts_pagination();
     }else{
